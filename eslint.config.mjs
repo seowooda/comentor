b8e84446ef39d738c1199bc217d9491bdf0ba846
seowooda <koeun0409@gmail.com>
2025-03-18 18:20:45 +0900

    chore: init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,46 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules/*', '.next/*', 'out/*'],
+    languageOptions: {
+      globals: {
+        browser: true,
+        node: true,
+        es6: true,
+      },
+    },
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-filename-extension': ['warn', { extensions: ['.ts', '.tsx'] }],
+      'no-useless-catch': 'off',
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
