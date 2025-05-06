e4c9e6b41728968ee209242011ddc0a71a9e6017
seowooda <koeun0409@gmail.com>
2025-05-07 00:45:26 +0900

    fix: 빌드 오류

--- src/app/github/page.tsx
+++
@@ -1,9 +0,0 @@
-import GitHubExplorer from '@/components/Github/github'
-
-export default function page() {
-  const owner = 'CommitMentor'
-  const repo = 'CoMentor-Frontend'
-  const branch = 'develop'
-
-  return <GitHubExplorer owner={owner} repo={repo} branch={branch} />
-}
