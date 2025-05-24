bfdd2cb3d8dc3300951903449750d05269d4919d
seowooda <koeun0409@gmail.com>
2025-05-24 22:53:53 +0900

    feat: 알림 목록, 알림 확인 API

--- src/api/services/notification/queries.ts
+++ src/api/services/notification/queries.ts
@@ -1,17 +1,22 @@
 import { fetcher, useGetQuery } from '@/api/lib/fetcher'
-import { DefaultResponse } from './model'
+import { DefaultResponse, Notification } from './model'
 import { useMutation } from '@tanstack/react-query'
 
 export const useNotifications = () => {
-  return useGetQuery<Notification>(['notifications'], '/notifications')
+  return useGetQuery<Notification[]>(['notifications'], '/notifications', {
+    enabled: false,
+  })
 }
 
 export const useReadNotification = () => {
-  useMutation<DefaultResponse, Error, number>({
+  return useMutation<DefaultResponse, Error, number>({
     mutationFn: (notificationId: number) => {
-      return fetcher<DefaultResponse>(`/notifications/${notificationId}`, {
-        method: 'POST',
-      })
+      return fetcher<DefaultResponse>(
+        `/notifications/read?id=${notificationId}`,
+        {
+          method: 'POST',
+        },
+      )
     },
   })
 }
